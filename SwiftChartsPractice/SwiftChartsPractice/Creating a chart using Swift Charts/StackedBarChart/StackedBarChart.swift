e7369efb9e841ba733c9bca07c8957a5e8ee5b8f
eensungkim <eensungkim@gmail.com>
2024-04-12 10:47:29 +0900

    feat: Customize your chart
    https://developer.apple.com/documentation/charts/creating-a-chart-using-swift-charts#Customize-your-chart

--- SwiftChartsPractice/SwiftChartsPractice/Creating a chart using Swift Charts/StackedBarChart/StackedBarChart.swift
+++ SwiftChartsPractice/SwiftChartsPractice/Creating a chart using Swift Charts/StackedBarChart/StackedBarChart.swift
@@ -18,9 +18,18 @@ struct StackedBarChart: View {
                     x: .value("Shape Type", $0.type),
                     y: .value("Total Count", $0.count)
                 )
+                // color 값에 따라 다른 색상으로 표현
+                // 프레임워크가 색상을 랜덤하게 할당
                 .foregroundStyle(by: .value("Shape Color", $0.color))
             }
         }
+        // data 와 색상의 관계 설정
+        .chartForegroundStyleScale([
+            "Green": .green,
+            "Purple": .purple,
+            "Pink": .pink,
+            "Yellow": .yellow
+        ])
     }
 }
 
