3f3dc733c1ce36b6e510d3425cd3a42de9cdadef
eensungkim <eensungkim@gmail.com>
2024-04-12 09:55:56 +0900

    config: 초기 세팅

---
+++ SwiftChartsPractice/SwiftChartsPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftChartsPractice
+//
+//  Created by Kim EenSung on 4/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftChartsPractice/SwiftChartsPractice/SwiftChartsPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsPracticeApp.swift
+//  SwiftChartsPractice
+//
+//  Created by Kim EenSung on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
